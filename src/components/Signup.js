34d95dfc6af727f6ac2dbc3f55e2bb31e8997d35
NtwaliHeritier <heritierntwali@gmail.com>
2021-04-06 13:03:55 +0200

    add axios to project

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Signup = () => {
   return ( 
-    <>
-    </>
+    <form>
+      <input type="text" name="first_name"/>
+      <input type="text" name="last_name"/>
+      <input type="text" name="username"/>
+      <input type="text" name="password"/>
+      <button type="submit">Register</button>
+    </form>
    );
 }
  
